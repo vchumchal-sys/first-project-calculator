e769ac55f3f119e5ecd10bf61cc69406633dead6
vchumchal-sys <v.chumchal@gmail.com>
2025-12-02 21:21:19 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,34 @@
+
+
+def numbers():
+    result=0
+    while True:
+        try:
+            first_number=float(input("Enter the first number\n"))
+            second_number=float(input("Enter the second number\n"))
+            operator=input("Choose the operation you want (+, -,*, /)\n")
+            if operator=="+":
+                result=first_number+second_number
+                print(f"Result is {result}")
+            elif operator=="-":
+                result=first_number-second_number
+                print(f"Result is {result}")
+            elif operator =="/" and second_number==0:
+                print("It is not possible to divide by zero")
+            elif operator=="/":
+                result=first_number/second_number
+                print(f"Result is {result}")
+            elif operator=="*":
+                result=first_number*second_number
+                print(f"Result is {result}")
+            else:
+                print("You can only use the following operators: +, -, /,*.")
+                
+
+            break
+        except ValueError:
+            print("The entered value is not a number!")
+
+    
+    
+numbers()
